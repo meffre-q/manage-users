0572e99eaaaa007dfa1223b3ff5b7fb6eb93f845
meffre-q <dagger7x9@gmail.com>
2017-01-23 16:14:17 +0100

    Resolvers for Graphql Done

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -1,18 +1,18 @@
 const resolvers = models => ({
-  Query: {
-    getUserById(root, { id }) {
-      return models.User.findById(id).then(res => res);
+    Query: {
+        getUserById(root, { id }) {
+            return models.User.findById(id).then(res => res);
+        },
+        getUserByUsername(root, { username }) {
+            return models.User.findOne({ username }).then(res => res);
+        },
     },
-    getUserByUsername(root, { username }) {
-      return models.User.findOne({ username }).then(res => res);
+    Mutation: {
+        createUser(root, args) {
+            const user = new models.User(args);
+            return user.save().then(res => res);
+        },
     },
-  },
-  Mutation: {
-    createUser(root, args) {
-      const user = new models.User(args);
-      return user.save().then(res => res);
-    },
-  },
 });
 
 module.exports = resolvers;

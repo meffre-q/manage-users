c7db8e52adeb474ecb48b106a463f2f3da4961cb
meffre-q <dagger7x9@gmail.com>
2017-01-23 16:12:51 +0100

    Add config file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  mongo: {
-    username: 'toto',
-    password: 'toto',
-    url: 'localhost:27017',
-    database: 'manage-users',
-  },
-  server: {
-    hostname: 'localhost',
-    port: 8080,
-  },
+    mongo: {
+        username: 'toto',
+        password: 'toto',
+        url: 'localhost:27017',
+        database: 'manage-users',
+    },
+    server: {
+        hostname: 'localhost',
+        port: 8080,
+    },
 };

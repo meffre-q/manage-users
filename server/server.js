6bf4867ac349d3c9a9e7cb53ede7a98e9ae636bb
meffre-q <dagger7x9@gmail.com>
2017-01-23 16:18:40 +0100

    Add catch for promise test

--- server/server.js
+++ server/server.js
@@ -6,14 +6,30 @@ const Config = require('./config/config');
 const server = new Hapi.Server();
 
 server.connection({
-  port: Config.server.port,
+    port: Config.server.port,
+    routes: {
+        cors: true,
+    },
 });
 
-server.route(Routes.endpoints);
-
-server.start((err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`Server running at: ${server.info.uri}`);
+server.register({
+    register: Routes,
+})
+.catch((err) => {
+    console.error('Routes registration error: ', err);
+})
+.then(() => {
+    server.on('response', (req) => {
+        console.info(`${req.info.remoteAddress}: ${req.method.toUpperCase()} ${req.url.path} --> ${req.response.statusCode}`);
+    });
+    server.on('route', (route) => {
+        console.info(`${route.method}\t${route.path}`);
+    });
+})
+.then(() => server.start())
+.catch((err) => {
+    console.error('Server error: ', err);
+})
+.then(() => {
+    console.info(`Server running at: ${server.info.uri}`);
 });

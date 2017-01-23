75e3d6f37955e078415f9ac709947ad33953a5b6
meffre-q <dagger7x9@gmail.com>
2017-01-23 16:15:27 +0100

    User model Done

--- server/models/user.js
+++ server/models/user.js
@@ -1,17 +1,17 @@
 const Mongoose = require('../database').Mongoose;
 
 const userSchema = new Mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    index: {
-      unique: true,
+    username: {
+        type: String,
+        required: true,
+        index: {
+            unique: true,
+        },
     },
-  },
-  description: {
-    type: String,
-    required: false,
-  },
+    description: {
+        type: String,
+        required: false,
+    },
 });
 const User = Mongoose.model('User', userSchema);
 exports.User = User;
